444a3193ef188df64f026b185ad6d1b02a654005
Sadiya-01 <61140814+Sadiya-01@users.noreply.github.com>
2020-04-29 13:18:43 +0530

    Few changes

---
+++ build/precache-manifest.44a4f5e1a054eb7a235b17ccb7ad8c0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c70ef2c968cfcb5911640ae15cc17e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e83c9a3de96ac7b387de",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8d0404f5a6f9c1a586bd",
+    "url": "/static/css/main.427e34f9.chunk.css"
+  },
+  {
+    "revision": "e83c9a3de96ac7b387de",
+    "url": "/static/js/2.8abce502.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8abce502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d0404f5a6f9c1a586bd",
+    "url": "/static/js/main.11da9bcd.chunk.js"
+  },
+  {
+    "revision": "5eeb774ecb6cfea4b9da",
+    "url": "/static/js/runtime-main.db9b5b05.js"
+  }
+]);
