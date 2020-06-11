ef608ebd67a19e0b0ec256789a63d0e5d3684072
Sadiya-01 <61140814+Sadiya-01@users.noreply.github.com>
2020-06-11 16:23:14 +0530

    api-doc

---
+++ build/precache-manifest.1166e3867d686c6d0dea187f9cc5c164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d4609f3fff760cac692023f7d07c57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83eba7019927c66f67a3",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f8922b08d757f9fe603b",
+    "url": "/static/css/main.60c2953d.chunk.css"
+  },
+  {
+    "revision": "83eba7019927c66f67a3",
+    "url": "/static/js/2.a89f370f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a89f370f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8922b08d757f9fe603b",
+    "url": "/static/js/main.524a9086.chunk.js"
+  },
+  {
+    "revision": "5eeb774ecb6cfea4b9da",
+    "url": "/static/js/runtime-main.db9b5b05.js"
+  }
+]);
